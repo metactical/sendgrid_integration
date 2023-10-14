add5420cc9a78593bbf08eeffb83f6371af50bcc
oderao <tripleo4u@gmail.com>
2023-10-14 07:46:15 +0100

    add sendgrid settings add sendgrid log

---
+++ sendgrid_integration/sendgrid_integration/doctype/sendgrid_log/sendgrid_log.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Nigmacorp and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('SendGrid Log', {
+	// refresh: function(frm) {
+
+	// }
+});
