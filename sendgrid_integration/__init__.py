f309fa3796e23ce8f3630e7a5ddc1cb6eab95c00
oderao <tripleo4u@gmail.com>
2023-11-02 12:56:46 +0100

    update webhook for sendgrid

--- sendgrid_integration/__init__.py
+++ sendgrid_integration/__init__.py
@@ -1,3 +1,11 @@
 
 __version__ = '0.0.1'
 
+from sendgrid_integration.overrides import custom_init_request,custom_make_form_dict
+
+from frappe.app import init_request , make_form_dict
+
+
+make_form_dict = custom_make_form_dict
+# if __name__ == '__main__':
+#     init_request = custom_init_request
--- sendgrid_integration/webhook.py
+++ sendgrid_integration/webhook.py
@@ -1,7 +1,30 @@
 import frappe
-
+from sendgrid_integration.utils import create_log
+import json
 @frappe.whitelist(allow_guest=True)
 def handle_sendgrid(*args,**kwargs):
-    frappe.log_error("args", args)
+    if kwargs and kwargs.get("request_data"):
+        email_list = kwargs.get("request_data")
+        email_dict = {"email_dict":email_list}
+        frappe.enqueue(process_sendgrid_event,queue="default",email_dict=email_dict)
+        
+        frappe.local.response['code'] = 200
+        frappe.local.response['message'] = "Webhook received and processed successfully"
+        
+ 
+        
+def process_sendgrid_event(email_dict:dict):
+    #create a sendgrid log
+    log = create_log("response",resource_type="Webhook Event")
+    log.api_response = json.dumps(email_dict, indent=4)
+    log.status = ""
     
-    frappe.log_error("kwargs", kwargs)
+    email_list = email_dict["email_dict"]
+    for obj in email_list:
+        #we are only interested in subscribe and unsubcribe events for now
+        #check if email is exsiting in the lead source table
+        if frappe.db.exists("Lead Source Table",{"contact_email":obj.get("email")}):
+            if obj.get("event") == "unsubscribe":
+                frappe.db.set_value("Lead Source Table", {"contact_email":obj.get("email")},"is_subscribed",0)
+    log.insert(ignore_permissions=True)
+    frappe.db.commit()
