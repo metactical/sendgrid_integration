04dc35c846fb8fdcf1d6a65c5e69d6c7a1eed16e
Aisenyi <malisa.aisenyi@gmail.com>
2024-08-07 17:40:13 +0300

    chore: Made changes to setup for frappe version-14 integration

--- sendgrid_integration/__init__.py
+++ sendgrid_integration/__init__.py
@@ -1,7 +1,7 @@
-
-__version__ = '0.0.1'
-
+import frappe
 from sendgrid_integration.overrides import custom_init_request
 import frappe.app
+
+__version__ = '0.0.1'
 
 frappe.app.init_request = custom_init_request
